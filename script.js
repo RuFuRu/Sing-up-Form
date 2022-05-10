a3705c8f48974d7c8a161237b8682e33dad0a826
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-05-10 16:04:42 +0200

    added script and stylysed the error message

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const passwd = document.getElementById('password');
 const confirmpasswd = document.getElementById('confirmPassword');
+const divs = document.querySelectorAll('.pass-div');
+const submit = document.querySelector('button');
+
+submit.addEventListener('click', (e) => {
+    if(passwd.value !== confirmpasswd.value) {
+        passwd.classList.add('error');
+        confirmpasswd.classList.add('error');
+        divs.forEach(div => {div.classList.add('div-error');console.log(div)});
+        e.preventDefault();
+    }
+})
+
 
 console.log(passwd, confirmpasswd);
